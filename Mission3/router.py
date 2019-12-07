cd8ed28aeca67700e06bc367b55cdf001ce334e7
Peter Maida <pmaida1357@gmail.com>
2019-12-07 14:58:39 -0500

    Code from friday

--- Mission3/router.py
+++ Mission3/router.py
@@ -1,9 +1,25 @@
 from netfilterqueue import NetfilterQueue
-#from scapy.all import *
+from scapy.all import *
 
 def print_and_accept(raw_pkt):
-        print(raw_pkt)
-        raw_pkt.accept()
+	pkt = IP(raw_pkt.get_payload())
+	print(pkt)
+	if DNSQR in pkt:
+		print('Before:')
+		print(pkt[DNSQR].show())
+		'''pkt[DNSQR].rdata = '10.4.10.3'
+		qname = pkt[DNSQR].qname
+		dns_response = DNSRR(rrname=qname, rdata='10.4.10.3', ttl=pkt[DNSRR].ttl)
+		pkt[DNSRR].an = dns_response
+		pkt[DNSRR].ancount = 1
+		del pkt[IP].len
+		del pkt[IP].chksum
+		del pkt[UDP].len
+		del pkt[UDP].chksum
+		print('After:')
+		print(pkt[DNSQR].show())
+		raw_pkt.set_payload(str(pkt))'''
+	raw_pkt.accept()
 
 
 nfqueue = NetfilterQueue()
--- Mission3/router.sh
+++ Mission3/router.sh
@@ -3,3 +3,5 @@ ifconfig eth0:1 10.4.10.130/26 up
 sysctl net.ipv4.ip_forward=1
 /etc/init.d/networking restart
 iptables -I FORWARD -j NFQUEUE --queue-num 1
+
+iptables -I OUTPUT -p udp --destination-port 53 -j NFQUEUE --queue-num 1
