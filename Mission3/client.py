9a5f10f647db9309baf488a70f7e2ce1e35e5bb3
Peter Maida <pmaida1357@gmail.com>
2019-12-07 15:23:05 -0500

    no indent error

--- Mission3/client.py
+++ Mission3/client.py
@@ -10,8 +10,8 @@ message = message.encode('hex')
 print(message)
 
 for b in range(0, len(message), 2):
-	send = 'a' + 'EF' + message[b] + message[b+1] + '.google.com'
-	print(send)
+    send = 'a' + 'EF' + message[b] + message[b+1] + '.google.com'
+    print(send)
     # inject 0 to 3 amounts of noise into the pipe
     for i in range(random.randint(0, 3)):
         socket.gethostbyname("google.com")
